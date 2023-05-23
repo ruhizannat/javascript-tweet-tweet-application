eeead00bdf052893741c442fb62d3860501d955d
ruhi <ruhizannat71@gmail.com>
2023-05-23 16:01:15 +0600

    Create a Word Counter

--- main.js
+++ main.js
@@ -8,14 +8,47 @@ const btnElm = document.querySelector('.submit');
 const dateElm = document.querySelector('.date-style');
 const msgElm = document.querySelector('.msg');
 
+let count;
+const num1 = 250;
+const num2 = 0;
+
+const wordCount = (text) => {
+	const arrayOfWords = text.split(' ');
+	count = 0;
+	for (let word of arrayOfWords) {
+		if (/[a-zA-Z0-9]/.test(word)) {
+			count += 1;
+		}
+	}
+
+	return count;
+};
+const setWordCount = (count) => {
+	// DOM update
+	limitElm.textContent = count;
+};
+
+const setInitialDOM = () => {
+	limitNumberElm.textContent = num1;
+	limitElm.textContent = num2;
+};
+
 const receiveInput = () => {
 	const inputValue = textElement.value;
 	return inputValue;
 };
+
 formElm.addEventListener('submit', (e) => {
 	//prevent browser reloading
 	e.preventDefault();
 	//receive the input value
 	const inputValue = receiveInput();
-	
 });
+
+textElement.addEventListener('input', (e) => {
+	const text = e.target.value.trim();
+
+	// get word count
+	setWordCount(wordCount(text));
+});
+setInitialDOM();
