7234c8d6ff8dea499086e0f64a22a3a75154fa0c
ruhi <ruhizannat71@gmail.com>
2023-05-23 16:19:47 +0600

    Working with Validation

--- main.js
+++ main.js
@@ -37,12 +37,53 @@ const receiveInput = () => {
 	const inputValue = textElement.value;
 	return inputValue;
 };
+const clearMsg = () => {
+	msgElm.textContent = '';
+};
+const showMessage = (msg, action) => {
+	const textMsg = `<div class="alert alert-${action} text-center" role="alert">
+  ${msg}
+</div>`;
+	msgElm.insertAdjacentHTML('afterbegin', textMsg);
+	setTimeout(() => {
+		clearMsg();
+	}, 2000);
+};
+const validatedInput = (inputValue) => {
+	let isValid = true;
+	//check input is empty
+	if (inputValue === '') {
+		isValid = false;
+		showMessage('please provide necessary info', 'danger');
+	}
+	if (inputValue.length > num1 && count > num1) {
+		isValid = false;
+		showMessage(
+			`You cannot put more than ${num1} words in this text area.`,
+			'danger'
+		);
+	}
+	return isValid;
+};
 
+const resetInputs = () => {
+	textElement.value = '';
+	limitElm.textContent = num2;
+	limitNumberElm.textContent = num1;
+};
 formElm.addEventListener('submit', (e) => {
 	//prevent browser reloading
 	e.preventDefault();
 	//receive the input value
 	const inputValue = receiveInput();
+
+	//check validation
+	const isValid = validatedInput(inputValue);
+	if (!isValid) return;
+
+	// reset the inputs
+	resetInputs();
+	console.log(inputValue);
 });
 
 textElement.addEventListener('input', (e) => {
